543a7ca0d99d1b27771bfa7d4ab11f1154e9fb74
siennatariman <tariman.sienna@gmail.com>
2024-01-17 19:58:38 +0800

    accepted solution-submitted

--- easy/1480.cpp
+++ easy/1480.cpp
@@ -1,10 +1,18 @@
-// Running Sum of 1d Array
+// 1480. Running Sum of 1d Array
 #include <iostream>
 
-using namespace std;
-
-int main()
-{
-	
-	return 0;
-}
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        std::vector<int> resultVector;
+        
+        for(int i=0; i < nums.size(); i++){
+            if(i==0){
+                resultVector.push_back(nums[i]);
+            } else {
+                resultVector.push_back(nums[i] + resultVector[i-1]);
+            };
+        };
+        return resultVector;
+    }
+};
