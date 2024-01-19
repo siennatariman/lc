7e5468533a571bc909b5adb95bc278df8430a112
siennatariman <tariman.sienna@gmail.com>
2024-01-19 20:27:56 +0800

    string interpolation will be used. will write in js instead of cpp

---
+++ easy/412.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+    std::vector<string> resultVector;
+
+    for(i=0;)
+
+
+
+
+    return resultVector;
+    }
+};
+
+// decided to forego using c++ in writing the solution since i realized 
+// that i need to use string interpolation. 
+// will be writing the soln. in javascript instead.
