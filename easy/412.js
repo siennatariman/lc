b70ac7371bac13b0df71a5dff3c1a15d6c91b43e
siennatariman <tariman.sienna@gmail.com>
2024-01-20 19:05:13 +0800

    accepted solution-submitted

--- easy/412.js
+++ easy/412.js
@@ -6,20 +6,20 @@
  */
 var fizzBuzz = function(n) {
     let resultArray =  [];
-
-    for(i=0; i < n; i++){
-        if((i%3==0) && (i%5==0) && i!==0){
-            resultArray[i] = "FizzBuzz"; 
-        } else if (i%3==0 && i!==0){
+    let m=1;
+    for(let i=0; i < n; i++){
+        if((m%3==0) && (m%5==0)){
+            resultArray[i] = "FizzBuzz";
+        } else if(m%3==0){
             resultArray[i] = "Fizz";
-        } else if (i%5==0 && i!==0) {
+        } else if(m%5==0){
             resultArray[i] = "Buzz";
-        } else if (i==0){
-            resultArray[i] == "1";
         } else {
-            resultArray[i] = `${i}`;
+            resultArray[i] = `${m}`;
         }
+        m++;
     }
 
     return resultArray;
 };
+
