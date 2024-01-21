e38386a76518629b543f45561d7f685ef4ad6635
siennatariman <tariman.sienna@gmail.com>
2024-01-21 21:14:55 +0800

    passed the two initial test cases but has not yet passed test case 13

---
+++ easy/2525.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+class Solution {
+public:
+    std::string categorizeBox(int length, int width, int height, int mass) {
+    std::string boxCategory1, boxCategory2, finalBoxCategory;
+    int volume = length * width * height;
+
+    if((length >= std::pow(10,4)) || (width >= std::pow(10,4)) || (height>= std::pow(10,4)) || (mass >= std::pow(10,4)) || (volume >= std::pow(10,9))){
+        boxCategory1 = "Bulky";
+    }
+
+    if(mass >= 100){
+        boxCategory2 = "Heavy";
+    }
+
+    if(boxCategory1 == "Bulky" && boxCategory2 == "Heavy"){
+        finalBoxCategory = "Both";
+    } else if (boxCategory1 != "Bulky" && boxCategory2 != "Heavy"){
+        finalBoxCategory = "Neither";
+    } else if (boxCategory1 == "Bulky" && boxCategory2 != "Heavy"){
+        finalBoxCategory = "Bulky";
+    } else if (boxCategory1 != "Bulky" && boxCategory2 == "Heavy") {
+        finalBoxCategory = "Heavy";
+    }
+
+    return finalBoxCategory;
+    }   
+};
