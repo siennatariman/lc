4c0e3b2e8f176d4bab06a58dcf3673588b35b310
siennatariman <tariman.sienna@gmail.com>
2024-02-09 20:51:29 +0800

    added a while loop so we can keep dividing or subtracting until the newNum reaches 0

--- easy/1342.cpp
+++ easy/1342.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
+    int newNum = 0;
+    int stepCount = 0;
+
     int numberOfSteps(int num) {
-        
+        newNum = num;
+        while(newNum!=0){
+            
+        }
     }
 };
