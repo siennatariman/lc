50518d307caf475c8715a008e9eb30db6eb1141e
siennatariman <tariman.sienna@gmail.com>
2024-02-09 20:53:23 +0800

    added an if-else statement to determine if num is odd/even, from there will execute the appropriate arithmetic operation

--- easy/1342.cpp
+++ easy/1342.cpp
@@ -6,7 +6,11 @@ public:
     int numberOfSteps(int num) {
         newNum = num;
         while(newNum!=0){
-            
+            if(newNum%2==0){ // number is even
+                newNum /= 2;
+            } else if (newNum%2!=0){ // number is odd
+                newNum -= 1;
+            }
         }
     }
 };
