20b4ab1d392d4a107506d8f7ce03c23c4527ea43
siennatariman <tariman.sienna@gmail.com>
2024-02-18 21:18:21 +0800

    added for loops to iterate over rows and columns of the 2d vector

--- easy/1672.cpp
+++ easy/1672.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
+    vector<vector<int>> customer;
     int maximumWealth(vector<vector<int>>& accounts) {
-        
+    customer = accounts;
+        // Iterate over the rowws
+      for(size_t i=0; customer.size(); i++){
+          // Iterate the columns for each row
+          for(size_t j=0; customer[i].size(); j++){
+
+          }
+      }  
+
     }
 };
