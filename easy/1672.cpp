c557299a10a7dd7d1fe832e88bef9cdfb0f75ffc
siennatariman <tariman.sienna@gmail.com>
2024-02-19 20:28:46 +0800

    1672. Richest Customer Wealth - Accepted Solution and passed all 3 test cases

--- easy/1672.cpp
+++ easy/1672.cpp
@@ -4,22 +4,24 @@ public:
     int container = 0;
 
     vector<vector<int>> customer;
-
+    
     int maximumWealth(vector<vector<int>>& accounts) {
-    customer = accounts;
-        // Iterate over the rowws
+        customer = accounts;
+
+        // Iterate over the rows
         for(size_t i=0; i < customer.size(); i++){
-          // Iterate the columns for each row
+
+            // Iterate the columns for each row
             for(size_t j=0; j < customer[i].size(); j++){
-            container += customer[i][j];
+                container += customer[i][j];
             }
             if(currentHighest < container){
                 currentHighest = container;
             }
+
         // Reset container for the next customer
         container = 0;
-
       }  
-
+    return currentHighest;
     }
 };
