1b04c1b9cb0318cdb6198be002058a850733e427
siennatariman <tariman.sienna@gmail.com>
2024-03-08 20:49:57 +0800

    instead of using console.log, i used return instead to output Hellow World -> this solved the problem of having an undefined output. i also removed the rest parameter syntax on the first anonymous function since it is unnecessary

--- easy/2667.js
+++ easy/2667.js
@@ -1,11 +1,11 @@
 /**
  * @return {Function}
  */
-var createHelloWorld = function(...args) {
+var createHelloWorld = function() {
     
 
     return function(...args) {
-        console.log("Hello World");
+        return "Hello World";
     }
 };
 
