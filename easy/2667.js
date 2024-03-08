d4f418ea80be5bdac9d88fc58d2cdac2e7b9b728
siennatariman <tariman.sienna@gmail.com>
2024-03-08 20:46:24 +0800

    when I run this  version the output is undefined for the two test cases

--- easy/2667.js
+++ easy/2667.js
@@ -1,14 +1,24 @@
 /**
  * @return {Function}
  */
-var createHelloWorld = function() {
+var createHelloWorld = function(...args) {
     
+
     return function(...args) {
-        
+        console.log("Hello World");
     }
 };
 
+createHelloWorld();
 /**
  * const f = createHelloWorld();
  * f(); // "Hello World"
+ */
+
+
+/**
+ * In JavaScript, the rest parameter syntax (...args)
+ * allows a function to accept an indefinite number of arguments as an array.
+ * When used inside a function's parameter list,
+ * it collects any remaining arguments into an array.
  */
