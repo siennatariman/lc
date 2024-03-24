052c9062d34a165ca1dd2ea347383c3f2bcc70ea
siennatariman <tariman.sienna@gmail.com>
2024-03-24 21:39:32 +0800

    buffer

--- easy/2665.js
+++ easy/2665.js
@@ -20,7 +20,7 @@ var createCounter = function(init) {
         init = storeInit;
         return init;
     };
-    
+    //
     return {
         increment: increment,
         decrement: decrement,
