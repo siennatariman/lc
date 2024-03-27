3df229773578ff0a3582875e579391a6794c355b
siennatariman <tariman.sienna@gmail.com>
2024-03-27 21:45:12 +0800

    trying to figure out how truthy value work and how i can implement them in the three given use cases

--- easy/2634.js
+++ easy/2634.js
@@ -4,5 +4,14 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    
-};
+    // filteredArr = []
+    if(fn){
+        
+    }
+
+
+}; 
+
+// filter function takes in an array AND a function 
+// the function that the filter function takes in has two arguments
+//namely, arr[i], 
