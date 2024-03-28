f18343cc17a2966708efb90b6af584e80cda9d05
siennatariman <tariman.sienna@gmail.com>
2024-03-28 21:27:30 +0800

    placed an if condition wherein if the value in the array is considered a truthy, it will be stored in the filtered array (block executed because the value is truthy)

--- easy/2634.js
+++ easy/2634.js
@@ -7,9 +7,9 @@ var filter = function(arr, fn) {
     let filteredArr = [];
 
     for(let i=0; i<arr.length;i++){
-        
-
-        
+        if(fn(arr[i])){
+            filteredArr.push(arr[i])
+        }
     }
     
     
