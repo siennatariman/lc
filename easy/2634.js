3ace0dba38156f1356de063ea58f5d6b83c2799a
siennatariman <tariman.sienna@gmail.com>
2024-04-01 20:44:27 +0800

    Case 2 finally works and was acceptedgit commit -m

--- easy/2634.js
+++ easy/2634.js
@@ -7,7 +7,7 @@ var filter = function(arr, fn) {
     let filteredArr = [];
 
     for(let i=0; i<arr.length;i++){
-        if(fn(arr[i])){
+        if(fn(arr[i], i)){
             filteredArr.push(arr[i])
         }
     }
