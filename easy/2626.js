47f3b667adf926b0999c5abb50d34647bec22901
siennatariman <tariman.sienna@gmail.com>
2024-04-09 22:17:00 +0800

    created an if conditional to evaluate if the length of the array is 0, and nested the for loop from my previous commit to the else statement

--- easy/2626.js
+++ easy/2626.js
@@ -7,7 +7,12 @@
 var reduce = function(nums, fn, init) {
     
     // Execute fn on each element of the array
-    for(let i = 0; i<nums.length; i++){
-        
+    if(nums.length == 0){
+        return init
+    } else {
+        for(let i = 0; i<nums.length; i++){
+
+        }
     }
+    
 };
