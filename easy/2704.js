964d287a87de495b01276b2cee4ec65e87bd9ec9
siennatariman <tariman.sienna@gmail.com>
2024-04-18 22:19:45 +0800

    replaced the existing conditional of var notToBe, we do not need to explicitly initialize a variable for null as the simple !== would suffice

--- easy/2704.js
+++ easy/2704.js
@@ -14,7 +14,7 @@ var expect = function(val) {
     };
 
     var notToBe = function(valTwo){
-        if((val ?? defaultValue) !== (valTwo ?? defaultValue)){
+        if(val!==valTwo){
             return true;
         } else {
             throw new Error("Equal");
@@ -31,3 +31,4 @@ var expect = function(val) {
  * expect(5).toBe(5); // true
  * expect(5).notToBe(5); // throws "Equal"
  */
+
