ac75be6ecb6298b455f273ecb750b3a797c70c9a
siennatariman <tariman.sienna@gmail.com>
2024-06-23 20:16:48 +0800

    added if-else block, declared currentInnerString variable

--- easy/14.js
+++ easy/14.js
@@ -6,7 +6,13 @@ var longestCommonPrefix = function(strs) {
     for (let i=0; i < strs.length; i++){
         let currentString = strs[i];
         for(let n=0; n < currentString.length; n++){
-            
+            if(){
+
+            } else {
+                
+            }
+            let currentInnerString = strs[i];
+        }
         
         
     }
